8dd93c482b3012fedcb8ae8897122cdfa6fb4d78
NandaGopal011 <nandadasgopal011@gmail.com>
2025-08-04 20:31:25 +0600

    second

--- number.js
+++ number.js
@@ -2,3 +2,15 @@ var orangePrice = 20;
 var chocolate = 0.5;
 console.log(typeof orangePrice);
 console.log(10>=10);
+
+
+var weight = 20;
+
+if(weight < 20){
+    console.log('I will carry it by myself');
+}
+
+else{
+    console.log('I will rent a rickshaw')
+}
+
---
+++ ternary.js
@@ -0,0 +1,6 @@
+const age =112;
+
+age >= 18 ? console.log('vote diyo'): console.log('ghumaiya thako')
+
+
+//price = isLeader === true ? 0 : price + 100 ;
